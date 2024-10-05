ebaba6d7129188202a05f98a5d8e1738e4a1c92a
krarktel112 <krarktel@gmail.com>
2024-10-05 18:50:47 -0400

    Update Email.py

--- Email.py
+++ Email.py
@@ -1,8 +1,8 @@
 # Importing libraries
 import imaplib, email
  
-user = 'USER_EMAIL_ADDRESS'
-password = 'USER_PASSWORD'
+user = 'krarktel@gmail.com'
+password = 'Yuka338'
 imap_url = 'imap.gmail.com'
  
 # Function to get email content part i.e its body part
