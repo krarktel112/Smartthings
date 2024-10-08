2f99292c43f081ddb1fc5d915d82a36285e60514
krarktel112 <krarktel@gmail.com>
2024-10-08 16:49:37 -0400

    Update Discordy.py

--- Discordy.py
+++ Discordy.py
@@ -4,7 +4,7 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
 
-    """async def on_message(self, message):
+    async def on_message(self, message):
         # don't respond to ourselves
         if message.author == self.user:
             return
@@ -12,15 +12,14 @@ class MyClient(discord.Client):
         if message.content == 'ping':
             await message.channel.send('pong')
 
-    async def on_presence_update(before: discord.Member, after: discord.Member):
+    """async def on_presence_update(before: discord.Member, after: discord.Member):
         if after.id == my_Member_id:
             print('{} changed status to {}'.format(
                 after.display_name,
                 after.status
             ))"""
 
-mytoken = input("Token:")
 intents = discord.Intents.default()
 intents.message_content = True
 client = MyClient(intents=intents)
-client.run(mytoken)
+client.run('MTI5Mjk2MzY2Mzc2NjA5ODAxMw.GaheI3.VKE5ISWmJ1Gwi5ksf1TlGsq9tapC60rZKe42gg')
