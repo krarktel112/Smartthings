971afeee1f240159744ca0ca342c27d84d547df4
krarktel112 <krarktel@gmail.com>
2024-10-09 17:58:22 -0400

    Update Smartthings.py

--- Smartthings.py
+++ Smartthings.py
@@ -1,23 +1,13 @@
-import asyncio
-from pysmartthings import SmartThings
+import aiohttp
+import pysmartthings
 
-async def main():
-    # Replace with your SmartThings personal access token
-    token = "372563b7-09f8-485b-8c95-261793424ad9"
+token = '372563b7-09f8-485b-8c95-261793424ad9'
 
-    # Create a SmartThings client
-    async with SmartThings("372563b7-09f8-485b-8c95-261793424ad9") as smartthings:
-        # Get all devices
-        devices = await smartthings.devices()
+async with aiohttp.ClientSession() as session:
+    api = pysmartthings.SmartThings(session, token)
+    locations = await api.locations()
+    print(len(locations))
 
-        # Print the name of each device
-        for device in devices:
-            print(device.label)
-
-        # Control a specific device (e.g., turn on a light)
-        light = next(dev for dev in devices if dev.label == "My Light")
-        if light:
-            await light.switch_on()
-
-if __name__ == "__main__":
-    asyncio.run(main())
+    location = locations[0]
+    print(location.name)
+    print(location.location_id)
