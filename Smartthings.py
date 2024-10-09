046beb7f188ad7831740029b0fa19fad5e52e382
krarktel112 <krarktel@gmail.com>
2024-10-09 18:03:17 -0400

    Update Smartthings.py

--- Smartthings.py
+++ Smartthings.py
@@ -1,13 +1,4 @@
-import aiohttp
-import pysmartthings
+import SmartThings
 
 token = '372563b7-09f8-485b-8c95-261793424ad9'
-
-async with aiohttp.ClientSession() as session:
-    api = pysmartthings.SmartThings(session, token)
-    locations = await api.locations()
-    print(len(locations))
-
-    location = locations[0]
-    print(location.name)
-    print(location.location_id)
+ST = SmartThings.Account(372563b7-09f8-485b-8c95-261793424ad9)
