d9ebd36c45a60bfc3af66a78da351db994b8f9da
krarktel112 <krarktel@gmail.com>
2024-10-10 17:43:34 -0400

    Update Discordy1.py

--- Discordy1.py
+++ Discordy1.py
@@ -3,6 +3,23 @@ import os
 
 import discord
 
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+    @client.event
+    async def on_ready():
+        for guild in client.guilds:
+            if guild.name == GUILD:
+                break
+
+        print(
+            f'{client.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})\n'
+        )
+
+        members = '\n - '.join([member.name for member in guild.members])
+        print(f'Guild Members:\n - {members}')
+
 
 TOKEN = input("Token:")
 intents = discord.Intents.default()
@@ -10,19 +27,3 @@ intents.message_content = True
 client = MyClient(intents=intents)
 client = discord.Client()
 client.run(TOKEN)
-
-@client.event
-async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})\n'
-    )
-
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
-
-
