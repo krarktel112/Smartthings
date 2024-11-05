44466ad00059a8417508f732032a64789db70301
krarktel112 <krarktel@gmail.com>
2024-11-04 21:34:48 -0500

    Update st.js

--- st.js
+++ st.js
@@ -1,5 +1,6 @@
 import { SmartThingsClient } from '@smartthings/core-sdk';
 
+token = '372563b7-09f8-485b-8c95-261793424ad9'
 const client = new SmartThingsClient({
   accessToken: 'YOUR_PERSONAL_ACCESS_TOKEN'
 });
