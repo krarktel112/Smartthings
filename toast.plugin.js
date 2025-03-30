1434ae8ae890c41ae88b49e3cd1413c09dad9b78
krarktel112 <krarktel@gmail.com>
2025-03-30 14:08:07 -0400

    Create toast.plugin.js

---
+++ toast.plugin.js
@@ -0,0 +1,3 @@
+    BdApi.showToast("This is a success message!", { type: "success" });
+    BdApi.showToast("Be careful!", { type: "warning" });
+    BdApi.showToast("An error occurred!", { type: "error" });
