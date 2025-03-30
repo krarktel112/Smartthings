3098403367e6e4b469fec6b7fb175ab4aad23570
krarktel112 <krarktel@gmail.com>
2025-03-30 14:45:47 -0400

    Update toast.plugin.js

--- toast.plugin.js
+++ toast.plugin.js
@@ -1,3 +1,24 @@
-    BdApi.showToast("This is a success message!", { type: "success" });
-    BdApi.showToast("Be careful!", { type: "warning" });
-    BdApi.showToast("An error occurred!", { type: "error" });
+/**
+ * @name ExamplePlugin
+ * @author YourName
+ * @description Describe the basic functions. Maybe a support server link.
+ * @version 0.0.1
+ */
+
+module.exports = class MyPlugin {
+  constructor(meta) {
+    // Do stuff in here before starting
+  }
+
+  start() {
+      BdApi.showToast("This is a success message!", { type: "success" });
+      BdApi.showToast("Be careful!", { type: "warning" });
+      BdApi.showToast("An error occurred!", { type: "error" });
+    // Do stuff when enabled
+  }
+
+  stop() {
+    // Cleanup when disabled
+  }
+};
+
