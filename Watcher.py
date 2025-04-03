215aafc9a32edbed2b22a1f27ab657138a2b4620
krarktel112 <krarktel@gmail.com>
2025-04-03 15:59:37 -0400

    Update Watcher.py

--- Watcher.py
+++ Watcher.py
@@ -4,13 +4,28 @@ from datetime import datetime
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
+def read_config_json(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            config_data= json.load(file)
+            return config_data
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+        return None
+    except json.JSONDecodeError:
+        print(f"Error: The file '{file_path}' contains invalid JSON.")
+    except  Exception as e:
+        print(f"An unexpected error occured: {e}")
+        return None
+config_file_path = r"C:\Users\User\AppData\Roaming\BetterDiscord\plugins\Aoffline.config.json"
+config = read_config_json(config_file_path)
 class FileOpenHandler(FileSystemEventHandler):
     def on_modified(self, event):
         if not event.is_directory:
             now = datetime.now()
             timestamp = datetime.timestamp(now)
             timestamp_str = now.strftime("%Y-%m-%d %H:%M:%S")
-            print(f"File opened: {event.src_path} {timestamp_str}")
+            print(f"File opened: {event.src_path} {timestamp_str} {config}")
 
 def watch_directory(path):
     observer = Observer()
