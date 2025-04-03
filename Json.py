10bd37a987c454a9efa9c21a7eac6911fa42dcbe
krarktel112 <krarktel@gmail.com>
2025-04-03 15:47:57 -0400

    Update Json.py

--- Json.py
+++ Json.py
@@ -1,11 +1,21 @@
 import json
 
-with open("data.json", "r") as file:
-    line = file.readline(-3)
+def read_config_json(file_path):
     try:
-        data = json.loads(line)
-        print(data["name"])
-        print(data["age"])
-        print(data["city"])
-    except json.JSONDecodeError as e:
-        print(f"Error decoding JSON: {e}")
+        with open(file_path, 'r') as file:
+            config_data= json.load(file)
+            return config_data
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+        return None
+    except json.JSONDecodeError:
+        print(f"Error: The file '{file_path}' contains invalid JSON.")
+    except  Exception as e:
+        print(f"An unexpected error occured: {e}")
+        return None
+config_file_path = 'C:\Users\User\AppData\Roaming\BetterDiscord\plugins\Aoffline.config.json'
+config = read_config_json(config_file_path)
+if config:
+    print(config)
+else:
+    print("Failed to load configuration.")
