fc096af40441bd9b0690fca39741ef3fb8631931
krarktel112 <krarktel@gmail.com>
2025-04-05 14:04:23 -0400

    Update Hi.py

--- Hi.py
+++ Hi.py
@@ -2,4 +2,9 @@ import SmartThings
 
 token = '372563b7-09f8-485b-8c95-261793424ad9'
 ST = SmartThings.Account(token)
+print("scenes")
 print(ST.scenes)
+print("devices")
+print(ST.devices)
+print("locations")
+print(ST.locations)
