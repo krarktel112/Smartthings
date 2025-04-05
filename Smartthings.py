6644f4bd12f76f3c6a31956c6e05c1dda0c8f844
krarktel112 <krarktel@gmail.com>
2025-04-05 14:09:06 -0400

    Update Smartthings.py

--- Smartthings.py
+++ Smartthings.py
@@ -2,9 +2,8 @@ import SmartThings
 
 token = '372563b7-09f8-485b-8c95-261793424ad9'
 ST = SmartThings.Account(token)
-print(ST.scenes)
 # Reference the SmartThings API documentation for information regarding the
 # format of capabilities, commands, and arguments
 
-ST.execute_scene(ST.scenes['Home']['Ashley1'])
+ST.CONTROL_DEVICE(ST.devices['Home']['Ashley1'], capability=switch, command=off(), arguments=None))
 
