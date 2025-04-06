50010cc8159229d1b77ed4323c941af289045dae
krarktel112 <krarktel@gmail.com>
2025-04-06 19:59:07 -0400

    Update Watcher.py

--- Watcher.py
+++ Watcher.py
@@ -30,29 +30,14 @@ class FileOpenHandler(FileSystemEventHandler):
             timestamp = datetime.timestamp(now)
             timestamp_str = now.strftime("%Y-%m-%d %H:%M:%S")
             config = read_config_json(config_file_path)
-            if (config == ("{'settings': {'setting1': 'online'}}")):
-                config == "online"
-                "ST.execute_scene(ST.scenes['Home']['Ashley2'])"
-            elif (config == ("{'settings': {'setting1': 'idle'}}")):
-                config == "idle"
-                "ST.execute_scene(ST.scenes['Home']['Ashley3'])"
-            elif (config == ("{'settings': {'setting1': 'dnd'}}")):
-                config == "dnd"
-            elif (config == ("{'settings': {'setting1': 'offline'}}")):
-                config == "offline"
-                "ST.execute_scene(ST.scenes['Home']['Ashley1'])"
-            elif (config == ("{'settings': {'setting1': 'listening'}}")):
-                config == "listening"
-            elif (config == ("{'settings': {'setting1': 'playing'}}")):
-                config == "playing"
-            elif (config == ("{'settings': {'setting1': 'streaming'}}")):
-                config == "streaming"
-            else:
-                config = read_config_json(config_file_path)
             print(f"File opened: {event.src_path} {timestamp_str} {config}")
             file_path = 'my_text_file.txt'
             with open(file_path, 'a') as file:
                 file.write(f"File opened: {event.src_path} {timestamp_str} {config}\n")
+            with open(file_path, "r") as file:
+                lines = file.readlines()
+                last_line = lines[-1]
+                
             ST.execute_scene(ST.scenes['Home']['Ashley1'])
 
 def watch_directory(path):
