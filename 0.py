ff7a1f3651eb234d7a5885dcfcf6828c961d569c
krarktel112 <krarktel@gmail.com>
2025-04-11 17:27:35 -0400

    Update 0.py

--- 0.py
+++ 0.py
@@ -15,7 +15,7 @@ import requests
 
 #try:
     #response = requests.get(base_url, params=payload)
-requests.get(base_url, params=payload)
+requests.get(https://api-v2.voicemonkey.io/trigger?token=b2ca9b305243e72ddc48196c3059e232_ed99b6d5eeeeb967306f6d3d703ecda2&device=echo1)
     #response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
     #print("Voice Monkey triggered successfully.")
 #except requests.exceptions.RequestException as e:
