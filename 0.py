510a5fda2f4bc063963866bf5569953396b439e4
krarktel112 <krarktel@gmail.com>
2025-04-11 17:51:34 -0400

    Update 0.py

--- 0.py
+++ 0.py
@@ -2,14 +2,14 @@ import requests
 
 base_url = "https://api.voicemonkey.io/trigger"
 access_token = "b2ca9b305243e72ddc48196c3059e232_ed99b6d5eeeeb967306f6d3d703ecda2"
-secret_token = "b2ca9b305243e72ddc48196c3059e232_ed99b6d5eeeeb967306f6d3d703ecda2&device=echo1"
+monkey = "b2ca9b305243e72ddc48196c3059e232_ed99b6d5eeeeb967306f6d3d703ecda2&device=echo1"
 #monkey = "<your_monkey_id>"
 #announcement = "<your_announcement_text>" # Optional: for announcement
 
 payload = {
     "access_token": access_token,
-    "secret_token": secret_token,
-#    "monkey": monkey,
+    #"se": secret_token,
+    "monkey": monkey,
 #    "announcement": announcement  # Include if you want to make an announcement
 }
 
