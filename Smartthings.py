4f0b663e0f180da92b319be9fc140852400bed1a
krarktel112 <krarktel@gmail.com>
2025-04-12 11:21:47 -0400

    Delete Smartthings.py

--- Smartthings.py
+++
@@ -1,8 +0,0 @@
-import SmartThings
-
-token = '372563b7-09f8-485b-8c95-261793424ad9'
-ST = SmartThings.Account(token)
-# Reference the SmartThings API documentation for information regarding the
-# format of capabilities, commands, and arguments
-
-ST.execute_scene(ST.scenes['Home']['Ashley1'])
