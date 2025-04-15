4ca3946e9aaccb79a9cc607327eef31a9fa5c925
krarktel112 <krarktel@gmail.com>
2025-04-15 00:24:41 -0400

    Update Watcher.py

--- Watcher.py
+++ Watcher.py
@@ -19,6 +19,7 @@ def read_config_json(file_path):
         print(f"An unexpected error occured: {e}")
         return None
 
+config_file_path = r"C:\Users\User\AppData\Roaming\BetterDiscord\plugins\Aoffline.config.json"
 config_file_path2 = r"C:\Users\User\AppData\Roaming\BetterDiscord\plugins\Aoffline.config.json"
 config = read_config_json(config_file_path)
 token = '372563b7-09f8-485b-8c95-261793424ad9'
