a0d22a9e6e2d1b3498db8eaea045ac4b70615ee7
krarktel112 <krarktel@gmail.com>
2025-05-24 17:16:11 -0400

    Update Plug2.js

--- Plug2.js
+++ Plug2.js
@@ -1,7 +1,7 @@
 // Using setInterval to change the bot's status every 10 seconds
 setInterval(() => {
     BdApi.Webpack.getStore("PresenceStore").getStatus('')
-    if 
-        (
+    if (x = online) {
+        
     });
 }, 1000);
