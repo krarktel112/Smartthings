8f0d4243d20613530d1477dddc88e7fdf5920244
krarktel112 <krarktel@gmail.com>
2025-05-24 15:46:14 -0400

    Create Plug.js

---
+++ Plug.js
@@ -0,0 +1,10 @@
+const activities = ["Playing a game", "Listening to music", "Watching a movie"];
+
+// Using setInterval to change the bot's status every 10 seconds
+setInterval(() => {
+    const randomActivity = activities[Math.floor(Math.random() * activities.length)];
+    bot.change_presence({
+        activities: [{ name: randomActivity, type: "PLAYING" }],
+        status: "online"
+    });
+}, 10000);
