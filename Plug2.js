eb269b693b9badc42e627d0ef2b49bd0cffd3b46
krarktel112 <krarktel@gmail.com>
2025-05-24 17:00:19 -0400

    Update Plug2.js

--- Plug2.js
+++ Plug2.js
@@ -1,8 +1,5 @@
 // Using setInterval to change the bot's status every 10 seconds
 setInterval(() => {
-    const randomActivity = activities[Math.floor(Math.random() * activities.length)];
-    bot.change_presence({
-        activities: [{ name: randomActivity, type: "PLAYING" }],
-        status: "online"
+    
     });
-}, 10000);
+}, 1000);
