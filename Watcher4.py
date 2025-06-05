f442e5da153d261c333e417b20ff6dd60016e83b
krarktel112 <krarktel@gmail.com>
2025-06-05 17:41:36 -0400

    Create Watcher4.py

---
+++ Watcher4.py
@@ -0,0 +1,131 @@
+import time
+import json
+from datetime import datetime
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import SmartThings
+
+def read_config_json(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            config_data= json.load(file)
+            return config_data
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+        return None
+    except json.JSONDecodeError:
+        print(f"Error: The file '{file_path}' contains invalid JSON.")
+    except  Exception as e:
+        print(f"An unexpected error occured: {e}")
+        return None
+
+config_file_path = r"C:\Users\User\AppData\Roaming\BetterDiscord\plugins\Asterea.config.json"
+config_file_path2 = r"C:\Users\User\AppData\Roaming\BetterDiscord\plugins\Asterea.config.json"
+config = read_config_json(config_file_path)
+token = '372563b7-09f8-485b-8c95-261793424ad9'
+ST = SmartThings.Account(token)
+y = "pop"
+
+class FileOpenHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        if not event.is_directory:
+            now = datetime.now()
+            timestamp = datetime.timestamp(now)
+            timestamp_str = now.strftime("%Y-%m-%d %H:%M:%S")
+            config = read_config_json(config_file_path)
+            x = event.src_path
+            #print(f"File opened: {event.src_path} {timestamp_str} {config}")
+            if x == config_file_path2:
+                file_path = 'my_text_file.txt'
+                with open(file_path, 'a') as file:
+                    file.write(f"Korra: {timestamp_str} {config}\n")
+                with open(file_path, "r") as file:
+                    lines = file.readlines()
+                    last_line = lines[-1]
+                    if "online" in last_line:
+                        if y == "online":
+                            y == "online"
+                        else:
+                            try:
+                                ST.execute_scene(ST.scenes['Home']['Ashley1'])
+                            except:
+                                print("Error")
+                            y == "online"
+                            print(f"Korra: {timestamp_str} {config}\n")
+                    if "idle" in last_line:
+                        if y == "idle":
+                            y == "idle"
+                        else:
+                            try:
+                                ST.execute_scene(ST.scenes['Home']['Ashley2'])
+                            except:
+                                print("Error")
+                            y == "idle"
+                            print(f"Korra: {timestamp_str} {config}\n")
+                    if "dnd" in last_line:
+                        if y == "dnd":
+                            y == "dnd"
+                        else:
+                            try:
+                                ST.execute_scene(ST.scenes['Home']['Ashley3'])
+                            except:
+                                print("Error")
+                            y == "dnd"
+                            print(f"Korra: {timestamp_str} {config}\n")
+                    if "offline" in last_line:
+                        if y == "offline":
+                            y == "offline"
+                        else:
+                            try:
+                                ST.execute_scene(ST.scenes['Home']['Ashley4'])
+                            except:
+                                print("Error")
+                            y == "offline"
+                            print(f"Korra: {timestamp_str} {config}\n")
+                    if "gaming" in last_line:
+                        if y == "gaming":
+                            y == "gaming"
+                        else:
+                            try:
+                                ST.execute_scene(ST.scenes['Home']['Ashley5'])
+                            except:
+                                print("Error")
+                            y == "gaming"
+                            print(f"Korra: {timestamp_str} {config}\n")
+                    if "listening" in last_line:
+                        if y == "listening":
+                            y == "listening"
+                        else:
+                            try:
+                                ST.execute_scene(ST.scenes['Home']['Ashley6'])
+                            except:
+                                print("Error")
+                            y == "listening"
+                            print(f"Korra: {timestamp_str} {config}\n")
+                    if "playing" in last_line:
+                        if y == "playing":
+                            y == "playing"
+                        else:
+                            try:
+                                ST.execute_scene(ST.scenes['Home']['Ashley7'])
+                            except:
+                                print("Error")
+                            y == "playing"
+                            print(f"Korra: {timestamp_str} {config}\n")
+
+def watch_directory(path):
+    observer = Observer()
+    event_handler = FileOpenHandler()
+    observer.schedule(event_handler, path, recursive=False)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+if __name__ == "__main__":
+    path_to_watch = r"C:\Users\User\AppData\Roaming\BetterDiscord\plugins"  # Replace with the directory you want to watch
+    watch_directory(path_to_watch)
