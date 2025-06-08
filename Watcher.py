2b95d752dd9d801140c4a5a28cad6bc0308172a0
krarktel112 <krarktel@gmail.com>
2025-06-07 21:29:26 -0400

    Update Watcher.py

--- Watcher.py
+++ Watcher.py
@@ -116,6 +116,83 @@ class FileOpenHandler(FileSystemEventHandler):
                                 print("Error")
                             y == "online"
                             print(f"Korra: {timestamp_str} {config}\n")
+            if x == config_file_path3:
+                file_path = 'Emma.txt'
+                with open(file_path, 'a') as file:
+                    file.write(f"Korra: {timestamp_str} {config}\n")
+                with open(file_path, "r") as file:
+                    lines = file.readlines()
+                    last_line = lines[-1]
+                    if "online" in last_line:
+                        if y == "online":
+                            y == "online"
+                        else:
+                            try:
+                                ST.execute_scene(ST.scenes['Home']['Ashley2'])
+                            except:
+                                print("Error")
+                            y == "online"
+                            print(f"Korra: {timestamp_str} {config}\n")
+                    if "idle" in last_line:
+                        if y == "online":
+                            y == "online"
+                        else:
+                            try:
+                                ST.execute_scene(ST.scenes['Home']['Ashley2'])
+                            except:
+                                print("Error")
+                            y == "online"
+                            print(f"Korra: {timestamp_str} {config}\n")
+                    if "dnd" in last_line:
+                        if y == "online":
+                            y == "online"
+                        else:
+                            try:
+                                ST.execute_scene(ST.scenes['Home']['Ashley2'])
+                            except:
+                                print("Error")
+                            y == "online"
+                            print(f"Korra: {timestamp_str} {config}\n")
+                    if "offline" in last_line:
+                        if y == "offline":
+                            y == "offline"
+                        else:
+                            try:
+                                ST.execute_scene(ST.scenes['Home']['Ashley3'])
+                            except:
+                                print("Error")
+                            y == "offline"
+                            print(f"Korra: {timestamp_str} {config}\n")
+                    if "gaming" in last_line:
+                        if y == "online":
+                            y == "online"
+                        else:
+                            try:
+                                ST.execute_scene(ST.scenes['Home']['Ashley2'])
+                            except:
+                                print("Error")
+                            y == "online"
+                            print(f"Korra: {timestamp_str} {config}\n")
+                    if "listening" in last_line:
+                        if y == "online":
+                            y == "online"
+                        else:
+                            try:
+                                ST.execute_scene(ST.scenes['Home']['Ashley2'])
+                            except:
+                                print("Error")
+                            y == "online"
+                            print(f"Korra: {timestamp_str} {config}\n")
+                    if "playing" in last_line:
+                        if y == "online":
+                            y == "online"
+                        else:
+                            try:
+                                ST.execute_scene(ST.scenes['Home']['Ashley2'])
+                            except:
+                                print("Error")
+                            y == "online"
+                            print(f"Korra: {timestamp_str} {config}\n")
 
 def watch_directory(path):
     observer = Observer()
